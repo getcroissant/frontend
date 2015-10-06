37d5611fb12c1b10ac98c6b53d47cada26f59eb0
Adam Chew <adamkchew@gmail.com>
2015-10-05 23:09:48 -0400

    Adding gulp and intialized the static content

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('www'));
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Application started listening at http://%s:%s', host, port);
+});
