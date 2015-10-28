f09dfea25aa90a079f0595ef0d3bbd75aefc191d
Adam Chew <adamkchew@gmail.com>
2015-10-28 17:13:55 -0400

    Adding documentation

--- public/scripts/filters/humanize.js
+++ public/scripts/filters/humanize.js
@@ -1,5 +1,10 @@
 var moment = require('moment');
 
+/**
+ * Takes an millisecond time input and converts it into words.
+ * @see http://momentjs.com/docs/#/durations/humanize/
+ * @returns {Function}
+ */
 module.exports = function() {
   return function(input) {
     return moment.duration(input).humanize();
