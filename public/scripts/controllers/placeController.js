abba31c253f0dc1103ab9f8b89f9ae08529d0ef9
Adam Chew <adamkchew@gmail.com>
2015-10-28 17:29:31 -0400

    Adding some related content for hiking

--- public/scripts/controllers/placeController.js
+++ public/scripts/controllers/placeController.js
@@ -5,16 +5,14 @@ module.exports = function($scope, $http, $routeParams, Place) {
   $scope.place = {
     name: 'Machu Picchu',
     description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
-    products: [
+    amenities: [
       {
-        name: 'Thin wheat noodles',
-        description: 'Healthy noodles that feed a family of 4.',
-        amount: 6.00
+        name: 'A carbon fiber walking stick',
+        description: 'Climbing material that gives extra support for climbers'
       },
       {
-        name: 'Naruto',
-        description: 'Fish cake with a circular design.',
-        amount: 2.00
+        name: 'Three day lunch',
+        description: 'Variety selection of food that will supply all your burned proteins'
       }
     ],
     duration: 500000000,
