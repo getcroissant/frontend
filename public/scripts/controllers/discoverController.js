1cf62cce03becb5a8ed5558f83f8dced351229ad
Adam Chew <adamkchew@gmail.com>
2015-10-28 17:33:54 -0400

    Adding more examples on discover page

--- public/scripts/controllers/discoverController.js
+++ public/scripts/controllers/discoverController.js
@@ -3,10 +3,27 @@ module.exports = function($scope) {
   $scope.places = [
     {
       name: 'Machu Picchu',
+      unique: 'machu-picchu',
       description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
     },
     {
       name: 'Machu Picchu',
+      unique: 'machu-picchu',
+      description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
+    },
+    {
+      name: 'Machu Picchu',
+      unique: 'machu-picchu',
+      description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
+    },
+    {
+      name: 'Machu Picchu',
+      unique: 'machu-picchu',
+      description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
+    },
+    {
+      name: 'Machu Picchu',
+      unique: 'machu-picchu',
       description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
     }
 
--- public/scripts/controllers/placeController.js
+++ public/scripts/controllers/placeController.js
@@ -4,6 +4,7 @@ module.exports = function($scope, $http, $routeParams, Place) {
 
   $scope.place = {
     name: 'Machu Picchu',
+    unique: 'machu-picchu',
     description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
     amenities: [
       {
