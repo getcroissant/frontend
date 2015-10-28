232f88c15ff22f85bfcff17150c6947fef51a879
Adam Chew <adamkchew@gmail.com>
2015-10-28 01:27:46 -0400

    Added local

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //Redirect for front end routes
 app.get('*', function(req, res) {
