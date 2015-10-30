ab126f752c2ab964f6fd38737ba1b35c295d919b
Adam Chew <adamkchew@gmail.com>
2015-10-30 00:52:13 -0400

    Adding responsive carousel

--- public/scripts/controllers/splashController.js
+++ public/scripts/controllers/splashController.js
@@ -19,7 +19,29 @@ module.exports = function($scope, $http, $location) {
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
-      centerMode: true
+      centerMode: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+
     });
   });
 
