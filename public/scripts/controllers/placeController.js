09f28a2ea28748accc8397716c4821faf597b81d
Adam Chew <adamkchew@gmail.com>
2015-11-03 17:23:40 -0500

    Adding some requirements

--- public/scripts/controllers/placeController.js
+++ public/scripts/controllers/placeController.js
@@ -6,6 +6,18 @@ module.exports = function($scope, $http, $routeParams, Place) {
     name: 'Machu Picchu',
     unique: 'machu-picchu',
     description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views. Its exact former use remains a mystery.',
+    requirements: [
+      {
+        name: "Water",
+        icon: "water",
+        description: "Please bring enough water for a 3 day hike"
+      },
+      {
+        name: "Hiking shoes",
+        icon: "hiking-shoes",
+        description: "Hiking shoes are mandatory because this is going to be a tenuous hike"
+      }
+    ],
     amenities: [
       {
         name: 'A carbon fiber walking stick',
