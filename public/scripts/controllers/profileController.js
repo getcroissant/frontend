7a2956ff49839f24e87a807d9bd67dc0b5087d02
Adam Chew <adamkchew@gmail.com>
2015-11-03 19:36:07 -0500

    Added profile binding

--- public/scripts/controllers/profileController.js
+++ public/scripts/controllers/profileController.js
@@ -1,7 +1,8 @@
 module.exports = function($scope, $http, $routeParams, Profile) {
 
   $scope.profile = {
-    name: 'John Smith'
+    name: 'John Smith',
+    bio: 'Walked the 7 kingdoms on foot'
   };
 
   console.log('Profile loaded')
